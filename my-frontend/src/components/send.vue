4dfdca3d54d4178f2ecd1a75d94a270f758d64a9
zheng ling jun <1807280540@qq.com>
2023-11-08 21:42:22 +0800

    vue

---
+++ my-frontend/src/components/send.vue
@@ -0,0 +1,32 @@
+<script>
+    import axios from 'axios'
+    var data = {
+        "name":"MLP",
+        "arguments":{
+        "epoch":-1,
+        "layer":{
+        "linear1":256,
+        "sigmoid":-1,
+        "ReLU1":-1,
+        "linear2":128,
+        "ReLU2":-1,
+        "linear3":-1
+      }
+    }
+    }
+    function click1button(){
+        axios.post('http://localhost:8080/model/send_model_selection',data).then(response=>{
+        console.log(response)
+        }).catch(error=>{
+            console.log(error)
+        })
+    }
+    var clicks = document.getElementById("button");
+    clicks.onclick = click1button;
+</script>
+<template>
+    <div id="button">23kdjfbutton</div>
+</template>
+<style>
+
+</style>
