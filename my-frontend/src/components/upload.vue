22ac6d2d6cceb46780785006d367634e9c6644b8
zheng ling jun <1807280540@qq.com>
2023-11-13 21:33:26 +0800

    前端vue

--- my-frontend/src/components/upload.vue
+++ my-frontend/src/components/upload.vue
@@ -1,119 +1,45 @@
-<!-- <script>
-    var uploadResult=document.getElementById("uploadResult")
-    function uploadFile() {
-        // console.log("nihao")
-        alert("nihaoya")
-        // var fileInput = document.getElementById('fileInput');
-        // var file = fileInput.files[0];
-        // if (!file) return; // 没有选择文件
-        // var xhr = new XMLHttpRequest();
-        // // 处理上传进度
-        // xhr.upload.onprogress = function(event) {
-        //     var percent = 100 * event.loaded / event.total;
-        //     uploadResult.innerHTML='上传进度：' + percent + '%';
-        // };
-        // // 当上传完成时调用
-        // xhr.onload = function() {
-        //     if (xhr.status === 200) {
-        //         uploadResult.innerHTML='上传成功'+ xhr.responseText;
-        //     }
-        // }
-        // xhr.onerror = function() {
-        //     uploadResult.innerHTML='上传失败';
-        // }
-        // // 发送请求
-        // xhr.open('POST', 'http://localhost:8080/model/sendFile?task=ImageClassification', true);
-        // var formData = new FormData();
-        // formData.append('file', file);
-        // xhr.send(formData);
-    }
-    function says(message) {
-        // axios.post('http://localhost:8080/model/send_model_selection',data,{
-        //     headers:{
-        //         'Content-Type':'application/json'
-        //     }
-        // }).then(response=>{
-        //     // console.log(response)
-        //     console.log(response)
-        // }).catch(error=>{
-        //     console.log(error)
-        // })
-        alert("nihao")
-    } 
-</script>
-<template>
-  <h3>文件上传</h3>
-  <input type="file" id="fileInput" name="fileInput" >
-  <span id="uploadResult"></span>
-  <button @click="uploadFile">点击上传</button>
-  <button @click="says">模型参数上传</button>
-</template>
-<style>
-
-</style> -->
-
-<!-- <template>
-  <input type="file" ref="upload" >
-</template>
-<script>
-    function upload(){
-       var objfile = this.$refs.upload.files[0];
-       var formData = new FormData();
-       formData.append('file',this.$refs.upload.files[0]);
-       var flag;
-       fileupload(formData).then(res=>{
-           if (res.code!=200){
-             console.log(res)
-             flag = true;
-           }else {
-             flag = false;
-           }
+<script setup>
+    function submit(){
+        let fd = new FormData();
+        fd.append("img",this.$refs.picx.files[0]);
+        fd.append("username","balabala");
+        axios({
+            url:"/f",
+            method:"post",
+            headers:{'Content-Type':'multipart/form-data'},
+            data:fd
+        }).then(function(response){
+            console.log(response.data)
         })
     }
-    import axios from 'axios'
-
-export function request(config) {
-  // 1.创建axios的实例
-  const instance = axios.create({
-    baseURL: 'http://localhost:8888',
-    //timeout: 10000,
-    headers: {
-      'Content-Type': "application/json;charset=utf-8",
-      'Authorization': window.sessionStorage.getItem("token")
-    },
-    changeOrigin: true
-  })
-    // 2.axios的拦截器
-    // 2.1.请求拦截的作用
-    instance.interceptors.request.use(config => {
-        return config
-    }, err => {
-        // console.log(err);
-    })
-
-    // 2.2.响应拦截
-    instance.interceptors.response.use(res => {
-        return res.data
-    }, err => {
-        console.log(err);
-    })
-    // 3.发送真正的网络请求
-    return instance(config)
-    }
-
 </script>
-<style>
-
-</style> -->
 <template>
-    <!-- <form id="myForm" action="http://localhost:8080/model/sendFile?task=ImageClassification">
-    文件上传：<input type="file" id="fileInput" name="fileInput" ><br>
-    <input type="button" onclick="myFunction()" value="提交表单">
-    </form> -->
+    <div class="submits">
+      <form> 
+        <input class="file-input" ref='picx' type="file"/>
+      </form>
+        <button @click="submit" style="margin-top:1.6%;width:50%;height:46px;font-size: 16px;">删除</button>
+        <button @click="submit" style="margin-top:1.6%;width:50%;height:46px;font-size: 16px;">上传</button>
+    </div>
 </template>
-<script>
-    function myFunction() {
-        var myform = document.getElementById("myForm");
-        console.log("ih")
-    }
-</script>
+<style scoped>
+.submits{
+  width:100%;
+  height:100%;
+  left:60px;
+}
+.submits .file-input{
+  outline-style: none;
+  border:1px solid #ccc;
+  border-radius: 3px;
+  padding:64px 30px;
+  font-size:14px;
+  font-family: "Microsoft soft";
+}
+.file-input:focus{
+  border-color: #66af39;
+  outline: 0;
+  -webkit-box-shadow:input 0 1px 1px rgba(0,0,0,0.075),0 0 8px rgba(102,175,233,0.6);
+  box-shadow: inset 0 1px 1px rgba(0,0,0,0.075),0 0 8px rgba(102,175,233,0.6);
+}
+</style>
